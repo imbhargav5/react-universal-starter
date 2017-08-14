3a50e72a60f559403e63ca331581964c22f529f1
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2017-08-14 20:23:01 +0530

    Add hot loading

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,12 +2,37 @@ import webpack from "webpack";
 import path from "path";
 
 export default env => {
+  // utilities to add features based on env
+
+  const addEntity = (add, entity) => {
+    return add ? entity : undefined;
+  };
+  const ifProd = entity => addEntity(env.prod, entity);
+  const ifDev = entity => addEntity(env.dev, entity);
+  const removeEmpty = array => array.filter(i => !!i);
+
+  // set contentBase
+  const contentBase = env.prod
+    ? __dirname + "/dist"
+    : __dirname + "/src/client";
+
   return {
-    entry: [path.join(__dirname, "src/client/index.js")],
+    entry: removeEmpty([
+      ifDev("react-hot-loader/patch"),
+      ifDev("webpack-dev-server/client?http://localhost:8080"),
+      ifDev("webpack/hot/only-dev-server"),
+      path.join(__dirname, "src/client/index.js")
+    ]),
     output: {
       path: path.join(__dirname, "dist"),
       publicPath: env.prod ? "/" : "http://localhost:8080/",
       filename: env.prod ? "[name].[chunkhash].js" : "[name].bundle.js"
+    },
+    devServer: {
+      hot: true,
+      publicPath: "http://localhost:8080/",
+      contentBase: contentBase,
+      historyApiFallback: true
     },
     module: {
       rules: [
@@ -19,14 +44,6 @@ export default env => {
         }
       ]
     },
-    resolve: {
-      // alias: {
-      //   "react/lib/ReactMount": "react-dom/lib/ReactMout",
-      //   handlebars: "handlebars/dist/handlebars.js",
-      //   "simplebar-css": "simplebar/dist/simplebar.css",
-      //   "react-select-css": "react-select/dist/react-select.css"
-      // },
-      modules: [path.join(__dirname, "src/client"), "node_modules"]
-    }
+    plugins: removeEmpty([ifDev(new webpack.HotModuleReplacementPlugin())])
   };
 };

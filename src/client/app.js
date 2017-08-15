42b385c4beaf4bb83ba08f1bc5df1041b84052be
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2017-08-15 15:59:13 +0530

    Fix server dynamic imports

--- src/client/app.js
+++ src/client/app.js
@@ -1,21 +1,11 @@
 import { Route, Link, Redirect, Switch } from "react-router-dom";
 import React, { Component } from "react";
 import { Provider } from "react-redux";
-import Bundle from "./bundle";
+import bundle from "./bundle";
 
-// components load their module for initial visit
-const Home = props =>
-  <Bundle load={() => import("./containers/Home/index.js")}>
-    {Home => <Home {...props} />}
-  </Bundle>;
-const About = props =>
-  <Bundle load={() => import("./containers/About/index.js")}>
-    {About => <About {...props} />}
-  </Bundle>;
-const Counter = props =>
-  <Bundle load={() => import("./containers/Counter/index.js")}>
-    {Counter => <Counter {...props} />}
-  </Bundle>;
+const Home = bundle(() => import("./containers/Home"));
+const About = bundle(() => import("./containers/About"));
+const Counter = bundle(() => import("./containers/Counter"));
 
 import store from "./store";
 
@@ -32,19 +22,15 @@ export default class App extends Component {
               <Link to="/about">About</Link>
             </li>
             <li>
-              <Link to="/aboutus">About Us</Link>
-            </li>
-            <li>
               <Link to="/counter">Counter</Link>
             </li>
           </ul>
           <hr />
-          <Route exact path="/" component={Home} />
           <Switch>
-            <Redirect from={"/aboutus"} to="about" />
-            <Route path="/about" component={About} />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/counter" component={Counter} />
           </Switch>
-          <Route path="/counter" component={Counter} />
         </div>
       </Provider>
     );
--- src/client/bundle.js
+++ src/client/bundle.js
@@ -1,36 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Bundle extends Component {
-  state = {
-    // short for "module" but that's a keyword in js, so "mod"
-    mod: null
-  };
+export default function bundle(getComponent) {
+  return class AsyncComponent extends React.Component {
+    static Component = null;
+    state = { Component: AsyncComponent.Component };
 
-  componentWillMount() {
-    this.load(this.props);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.load !== this.props.load) {
-      this.load(nextProps);
+    componentDidMount() {
+      if (!this.state.Component) {
+        getComponent()
+          .then(module => module.default || module)
+          .then(Component => {
+            AsyncComponent.Component = Component;
+            this.setState({ Component });
+          });
+      }
     }
-  }
-
-  load(props) {
-    this.setState({
-      mod: null
-    });
-    props.load(mod => {
-      this.setState({
-        // handle both es imports and cjs
-        mod: mod.default ? mod.default : mod
-      });
-    });
-  }
-
-  render() {
-    return this.state.mod ? this.props.children(this.state.mod) : null;
-  }
+    render() {
+      const { Component } = this.state;
+      if (Component) {
+        return <Component {...this.props} />;
+      }
+      return null;
+    }
+  };
 }
-
-export default Bundle;
